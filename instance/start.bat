2ce44ebfe164b29351f350a59b6481763efeee38
eric8376 <8376eric@163.com>
2013-08-11 16:31:28 +0800

    change bureau scr path

--- instance/start.bat
+++ instance/start.bat
@@ -1,5 +1,5 @@
 set CATALINA_HOME=F:\tomcat\tomcat6
-set CATALINA_BASE=F:\eric\workspace\proj\WebDevs\WebDev\instance
+set CATALINA_BASE=F:\eric\workspace\proj\WebDevelope\instance
 cd\
 call F:\tomcat\tomcat6\bin\startup.bat 
 pause
